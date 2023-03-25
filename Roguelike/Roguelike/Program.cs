a83750932b636d27a15096408afcdd0586074b32
YugPancake <yug.blet@gmail.com>
2023-03-25 13:24:06 +1000

    герои и враги

--- Roguelike/Roguelike/Program.cs
+++ Roguelike/Roguelike/Program.cs
@@ -74,20 +74,24 @@ namespace Roguelike
         Character p1;
         List<Character> enemies;
 
-        class doubleArray
+        class Rooms
         {
             public double[,] data;
         }
-        List<doubleArray> Rooms;
+        List<Rooms> rooms;
 
-        public void createGame()
+        public void createEnemies(int classE)
         {
-            p1 = new Player(3, 1, 2, 1);
-            for(int i = 1; i <= 3; i++)
+            for(int i = 1; i <= 5; i++)
             {
-                var e1 = new Enemy();
+                var e1 = new Enemy(1, 1, 1, classE);
                 enemies.Add(e1);
             }
+        }
+
+        public void createPlayer(int classP)
+        {
+            var p1 = new Player(1, 1, 1, classP);
         }
 
     }
@@ -120,185 +124,3 @@ namespace Roguelike
             p1.hp -= 10;
         }
     }
-
-    internal class Program
-    {
-        bool quit = false;
-        int X;
-        int Y;
-
-        int[,] GameField;
-        int w = 40, h = 20;
-        int lavel = 0;
-        void Init()
-        {
-            Console.CursorVisible = false;
-            Console.SetWindowSize(w + 10, h + 10);
-            Console.SetBufferSize(w + 10, h + 10);
-        }
-
-        void Load(int level = 1)
-        {
-
-            X = w / 2;
-            Y = h / 2;
-            GameField = new int[w + 1, h + 1];
-            GameField[X, Y] = 1;
-            //Random random = new Random();
-            for (int i = 0; i <= w; i++)
-            {
-                GameField[i, 0] = 9;
-                GameField[i, h] = 9;
-            }
-            for (int i = 0; i < h; i++)
-            {
-                GameField[0, i] = 9;
-                GameField[w, i] = 9;
-            }
-
-        }
-        void moveGame(int d)
-        {
-            for (int i = 0; i <= w; i++)
-
-                for (int j = 0; j < h; j++)
-                {
-                    if (GameField[i, j] == 1)
-                    {
-                        switch (d)
-                        {
-                            case 1:
-
-
-                                if (GameField[i - 1, j] == 0)
-                                {
-                                    GameField[i, j] = 0;
-                                    GameField[i - 1, j] = 1;
-                                }
-                                return;
-
-
-                            case 2:
-
-
-                                if (GameField[i + 1, j] == 0)
-                                {
-                                    GameField[i, j] = 0;
-                                    GameField[i + 1, j] = 1;
-                                }
-                                return;
-
-                            case 3:
-
-                                if (GameField[i, j - 1] == 0)
-                                {
-                                    GameField[i, j] = 0;
-                                    GameField[i, j - 1] = 1;
-                                }
-                                return;
-
-                            case 4:
-
-                                if (GameField[i, j + 1] == 0)
-                                {
-                                    GameField[i, j] = 0;
-                                    GameField[i, j + 1] = 1;
-                                }
-                                return;
-
-
-
-
-                        }
-                    }
-
-                }
-        }
-        void PrintGameField()
-        {
-            for (int y = 0; y <= h; y++)
-                for (int x = 0; x <= w; x++)
-                {
-                    Console.SetCursorPosition(x, y + 1);
-
-                    switch (GameField[x, y])
-                    {
-                        case 0:
-                            Console.WriteLine(' ');
-                            break;
-                        case 1:
-                            Console.WriteLine('@');
-                            break;
-                        case 10:
-                            Console.WriteLine('/');
-                            break;
-                        default:
-                            Console.WriteLine('#');
-                            break;
-
-                    }
-                }
-            Console.SetCursorPosition(10, 0);
-
-        }
-        int keybordUpdate()
-        {
-            while (true)
-            {
-                ConsoleKeyInfo key;
-                key = Console.ReadKey(true);
-
-                switch (key.Key)
-                {
-                    case ConsoleKey.S:
-                        return 1;
-                    case ConsoleKey.D:
-                        return 2;
-                    case ConsoleKey.W:
-                        return 3;
-                    case ConsoleKey.A:
-                        return 4;
-                    case ConsoleKey.Escape:
-                        return 99;
-                    default:
-                        break;
-
-
-
-                }
-
-            }
-
-
-
-        }
-        public void Game()
-        {
-            Init();
-            Load();
-            PrintGameField();
-            int d = 0;
-            while (d != 99)
-            {
-                d = keybordUpdate();
-                moveGame(d);
-                PrintGameField();
-
-                //Console.WriteLine("действие "+d);
-            }
-
-
-
-
-
-
-        }
-
-        static void Main(string[] args)
-        {
-            Program program = new Program();
-            program.Game();
-            Console.ReadKey();
-        }
-    }
-}
